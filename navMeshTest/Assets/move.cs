cfd693c74cde113efb2dc8928cd0f583c3c36218
Nico <Ncollie42@gmail.com>
2020-03-04 16:55:09 -0800

    navMesh

---
+++ navMeshTest/Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();       
+        agent.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
